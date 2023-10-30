15b92f3b7106e4b6a9200c1f34f090f990db4299
Andrew <andrewstaveley14@gmail.com>
2023-10-29 23:20:52 -0400

    Added myMap, myReduce, myFind, myFilter. Not done

--- index.js
+++ index.js
@@ -1,6 +1,66 @@
-const myEach = function(array, alert) {
-    for (let num of Object.values(array)) {
-        alert(num)
+const myEach = function(collection, alert) {
+    for (let num of Object.values(collection)) {
+        alert(num);
     };
-    return array;
+    return collection;
+};
+const myMap = function(collection, callback) {
+    let newCollection = [];
+    myEach(collection, (x) => newCollection.push(callback(x)));
+    return newCollection;
+};
+const myReduce = function(collection, callback, acc) {
+    let startIdx = 0;
+  
+    if (acc === undefined) {
+      if (Array.isArray(collection)) {
+        if (collection.length === 0) {
+          throw new Error('Cannot reduce an empty array without a start value.');
+        }
+        acc = collection[0];
+        startIdx = 1;
+      } else if (typeof collection === 'object' && collection !== null) {
+        const keys = Object.keys(collection);
+        if (keys.length === 0) {
+          throw new Error('Cannot reduce an empty object without a start value.');
+        }
+        const firstKey = keys[0];
+        acc = collection[firstKey];
+        startIdx = 1;
+      } else {
+        throw new TypeError('Invalid collection type. Expected an array or an object.');
+      }
+    }
+  
+    if (Array.isArray(collection)) {
+      for (let i = startIdx; i < collection.length; i++) {
+        acc = callback(acc, collection[i], collection);
+      }
+    } else if (typeof collection === 'object' && collection !== null) {
+      const keys = Object.keys(collection);
+      for (let i = startIdx; i < keys.length; i++) {
+        const key = keys[i];
+        acc = callback(acc, collection[key], collection);
+      }
+    } else {
+      throw new TypeError('Invalid collection type. Expected an array or an object.');
+    }
+  
+    return acc;
+};
+const myFind = function(collection, callback) {
+    for (let value of Object.values(collection)) {
+        if (callback(value) === true) {
+            return value;
+        };
+    };
+};
+const myFilter = function(collection , callback) {
+    let finalArray = [];
+    for (let value of Object.values(collection)) {
+        if (callback(value) === true) {
+            finalArray.push(value);
+        };
+    };
+    return finalArray;
 };
